716904a176e86a66d798d328d927e5552e7e26f8
Suhas242 <77232505+Suhas242@users.noreply.github.com>
2023-01-29 17:16:52 +0530

    Add files via upload

---
+++ Assignment5.java
@@ -0,0 +1,33 @@
+package Assignments;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		// (ass 5)
+		int n=14;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0||i+j<=(n-1)/2||(i-j>=(n-1)/2)||i==n-1)				{
+					System.out.print("*");
+					
+				}
+				
+				else
+				{
+					System.out.print(" ");
+				}
+					
+					
+			}
+			System.out.println();
+				
+		}
+		
+		
+
+	}
+	
+}
+
